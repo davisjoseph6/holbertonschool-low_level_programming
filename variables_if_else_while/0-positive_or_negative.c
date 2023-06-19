dd54907b1057973dd590803de2bbfdea2f0eaba0
Davis <davisjoseph767@gmail.com>
2023-06-19 04:44:00 -0700

    0. Hello World

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -12,15 +13,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-	printf ("%d is positive\n", n);
+	printf("%d is positive\n", n);
 	}
 	else if (n = 0)
 	{
-	printf ("%d is is zero\n", n);
+	printf("%d is is zero\n", n);
 	}
 	else
 	{
-	printf ("%d is is negative", n);
+	printf("%d is is negative", n);
 	}
 	return (0);
 }
