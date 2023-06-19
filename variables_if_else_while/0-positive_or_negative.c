5f3d08890df8755e2be2e3e9286aebb2dffe029d
Davis <davisjoseph767@gmail.com>
2023-06-19 04:49:33 -0700

    0. Hello World

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,11 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-	printf("%d is is zero\n", n);
+	printf("%d is zero\n", n);
 	}
 	else
 	{
-	printf("%d is is negative", n);
+	printf("%d is negative", n);
 	}
 	return (0);
 }
