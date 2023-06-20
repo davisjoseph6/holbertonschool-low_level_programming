7bde679a199bb69aba5b172c46fc3b87eeafb169
Davis <davisjoseph767@gmail.com>
2023-06-20 07:07:10 -0700

    0. Hello World

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,29 +1,21 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-    char text[] = "_putchar\n";
-    int i;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-    for (i = 0; text[i] != '\0'; i++)
-        _putchar(text[i]);
-
-    return 0;
-}
-
-/**
- * _putchar - Writes a character to the standard output (stdout)
- * @c: The character to be written
- *
- * Return: On success 1
- * On error, -1 is returned, and errno is set appropriately
- */
-int _putchar(char c)
-{
-    return (write(1, &c, 1));
+	return (0);
 }
