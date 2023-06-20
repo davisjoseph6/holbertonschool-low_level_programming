98f5e3ba38907396b390750f7ecf96008d47ab48
Davis <davisjoseph767@gmail.com>
2023-06-20 07:51:35 -0700

    0. Hello World

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet - Make the alphabet
- *
- * Return: void
- */
+  * print_alphabet - Make the alphabet
+  *
+  * Return: void
+  */
 void print_alphabet(void)
 {
-  char c;
+	char c;
 
-  for (c = 'a'; c <= 'z'; c++)
-  {
-    _putchar(c);
-  }
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
 
-  _putchar('\n');
+	_putchar('\n');
 }
