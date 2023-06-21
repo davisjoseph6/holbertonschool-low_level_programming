7acb2561b4b4fc959c76e69a7dcee911e6bbb26e
Davis <davisjoseph767@gmail.com>
2023-06-21 02:19:42 -0700

    0. Hello World

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *void print_alphabet_x10 to print alphabet 10 times
+ *10 x alphabet
+ *return: void
+ */
+
+void print_alphabet_x10(void)
+{
+  int num = 0
+    char i;
+
+  for (i = 'a'; i <= 'z'; i++)
+    {
+      _putchar(i);
+      for (num = 0; num <= 9; num ++)
+	_putchar(num);
+        _putchar('\n');
+    }
+}
