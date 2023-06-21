886e36994444578c26711027dc46e7f58cd6b447
Davis <davisjoseph767@gmail.com>
2023-06-21 06:56:03 -0700

    0. Hello World

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,26 +1,26 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_sign - Prints the sign of a number.
- * @n: The number to check.
- *
- * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero.
- */
+  * print_sign - Prints the sign of a number
+  * @n: The number to be checked
+  *
+  * Return: 1 for positive num, -1 for negative num or zero for anything else
+  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return 1;
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return 0;
-    }
-    else
-    {
-        _putchar('-');
-        return -1;
-    }
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
 }
