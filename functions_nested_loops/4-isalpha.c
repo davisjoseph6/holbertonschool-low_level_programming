743f830b6f663ba1d1bd36b33e29e3935288dff9
Davis <davisjoseph767@gmail.com>
2023-06-21 06:33:00 -0700

    0. Hello World

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,16 +1,17 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _isalpha - Checks for alphabetic character
- * @c: The character to be expected
- *
- *Return: 1 for alphabetic character or 0 for anything else
- *
- */
+  * _isalpha - Checks for alphabetic character
+  * @c: The character to be checked
+  *
+  * Return: 1 for alphabetic character or 0 for anything else
+  */
 int _isalpha(int c)
 {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        return 1;
-    else
-        return 0;
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+
+	return (0);
 }
