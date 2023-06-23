43a9106f178b304b29cbf6b7d9b16755c2b8d0e3
Davis <davisjoseph767@gmail.com>
2023-06-23 11:37:06 -0700

    World

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+/*
+ * File: 5-more_numbers.c
+ * Auth: Davis Joseph
+ */
+
+#include "main.h
+#include <stdio.h>
+
+
+/**
+ * more_numbers - Prints the numbers 0-14 ten times.
+ */
+void more_numbers(void)
+{
+	int num, count;
+
+	for (count = 0; count <= 9; count++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+				putchar((num / 10) + '0');
+			putchar((num % 10) + '0');
+		}
+		putchar('\n');
+	}
+}
