e131f9ab7bd9ca2c59ed90e5d9577eabfafc96c9
Davis <davisjoseph767@gmail.com>
2023-06-27 03:25:49 -0700

    3. I do not fear computers. I fear the lack of them

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,22 @@
-/*
- * File: 4-print_rev.c
- * Auth: Davis Joseph
- */
-
 #include "main.h"
-
 /**
- * print_rev - Prints a string in reverse.
- * @s: The string to be printed.
+ * print_rev - print a string in reverse followed by a new line
+ * @s: string to be reversed
  */
+
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int i;
 
-	while (s[index++])
-		len++;
+	i = 0;
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
+	while (s[i] != '\0')
+		i++;
+	i--;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
 	_putchar('\n');
 }
