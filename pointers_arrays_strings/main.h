0bbb11eb46247a8ac3d335a36a611008a3d29968
Davis <davisjoseph767@gmail.com>
2023-06-27 03:18:29 -0700

    3. I do not fear computers. I fear the lack of them

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,5 +10,3 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int _putchar(char c);
-
-#endif /* MAIN_H */
