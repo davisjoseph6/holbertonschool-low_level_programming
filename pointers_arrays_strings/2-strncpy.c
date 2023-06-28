21262af1e8d748a06f8d6cd1d34ff09f95dd8f5d
Davis <davisjoseph767@gmail.com>
2023-06-28 05:21:57 -0700

    0. strcat

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,32 +1,22 @@
 #include "main.h"
-#include <string.h>
-
 
 /**
-  * _strncat - Concatenates two strings
-  * @dest: The destination value
-  * @src: The source value
-  * @n: The limit of the concatenation
-  *
-  * Return: A pointer to the resulting string dest
-  */
-char *_strncat(char *dest, char *src, int n)
+ * _strncpy - Copies a string
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The maximum number of characters to copy
+ *
+ * Return: Pointer to the destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
 {
-	int dlen = 0, j = 0;
+	int i;
 
-	while (dest[dlen])
-	{
-		dlen++;
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-	while (j < n && src[j])
-	{
-		dest[dlen] = src[j];
-		dlen++;
-		j++;
-	}
-
-	dest[dlen + n + 1] = '\0';
+	for (; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
