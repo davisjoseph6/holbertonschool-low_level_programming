13ad33800e2452d52870b97989bb3737c45076c5
Davis <davisjoseph767@gmail.com>
2023-06-28 20:54:27 -0700

    0. Hello World

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,37 +1,34 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * rev_string - Reverses a string.
+ * @s: The string to be reversed.
+ */
 void rev_string(char *s)
 {
     if (s == NULL)
         return;
 
     int length = 0;
+    char *start = s;
     char *end = s;
     char temp;
 
-    /* Find the length of the string */
+    // Find the length of the string
     while (*end != '\0') {
         length++;
         end++;
     }
 
-    /* Reverse the string by swapping characters */
+    // Reverse the string by swapping characters
     end--;
-    while (s < end) {
-        temp = *s;
-        *s = *end;
+
+    while (start < end) {
+        temp = *start;
+        *start = *end;
         *end = temp;
-        s++;
+
+        start++;
         end--;
     }
-}
-
-int main(void)
-{
-    char s[10] = "My School";
-
-    printf("%s\n", s);
-    rev_string(s);
-    printf("%s\n", s);
-    return 0;
 }
