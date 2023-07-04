77caaa883bd81d3d2b26c83641191b5ca8e8ce42
Davis <davisjoseph767@gmail.com>
2023-07-04 06:09:17 -0700

    0. Hello World

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -4,12 +4,11 @@
  * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed.
  */
-
 void _print_rev_recursion(char *s)
 {
-  if (*s)
-    {
-      _print_rev_recursion(s+1);
-      _putchar(*s);
-    }
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
