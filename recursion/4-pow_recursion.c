739d56397f044e82a1464b4a8a6aa688df150fd5
Davis <davisjoseph767@gmail.com>
2023-07-06 01:36:52 -0700

    0. Hello World

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,16 +1,17 @@
 #include "main.h"
-
+/**
+ * _pow_recursion - raises x to the power of y
+ * @x: Number Integer
+ * @y: Power
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _pow_recursion(int x, int y)
 {
-  int result = x;
-
-  if (y > 0)
-    return (-1);
-
-  else if (y == 0)
-    return (1);
-
-  result *= _pow_recursion(x, y -1);
-
-  return (result);
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
--- recursion/main.h
+++ recursion/main.h
@@ -1,3 +1,7 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -5,4 +9,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int _putchar(char c);
+
+#endif
