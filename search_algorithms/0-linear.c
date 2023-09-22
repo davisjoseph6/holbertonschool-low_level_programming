f3ae2e9511ce1ca5bff26386051ebb0e01bf4562
Davis <davisjoseph767@gmail.com>
2023-09-22 08:34:19 -0700

    added betty documentation style

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -2,12 +2,13 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Searches for a value in 
- *
- *
- *
- *
- *
+ * linear_search - Searches for a value in in an array of integers
+ * using the Linear search algorithm
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value:  the value to search for
+ * Return: If value is not present in array or if array is NULL, your function must return -1
+ * else return 0
  */
 int linear_search(int *array, size_t size, int value)
 {
